aaf5e18c6e01bd86a2de0fea872da7a9c8db19d0
Shruti Awasthi <shruti.awasthi@valtech.com>
2025-02-19 12:10:47 +0530

    HOC component update the user

---
+++ system-design-patterns/src/components/HOC-App Component/include-updatable-user.jsx
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+export const includeUpdatableUser = (Component, userId) => {
+    return (props) => {
+        const [initialUser, setInitialUser] = useState(null);
+        const [user, setUser] = useState(null);
+
+        useEffect(() => {
+            (async () => {
+                const response = await axios.get(`/users/${userId}`);
+                setInitialUser(response.data);
+                setUser(response.data);
+            })();
+        }, [])
+
+        const onChangeUser = updates => {
+            setUser({ ...user, ...updates });
+        }
+
+        const onPostUser = async () => {
+            const response = await axios.post(`/users/${userId}`, { user });
+            setInitialUser(response.data);
+            setUser(response.data);
+        }
+
+        const resetUser = () => {
+            setUser(initialUser);
+        }
+        return <Component {...props} user={user} onChangeUser={onChangeUser} onPostUser={onPostUser} resetUser={resetUser} />
+    }
+}
